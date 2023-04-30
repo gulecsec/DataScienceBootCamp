35a50834dfeef091d7bd91df80f1f261652fa2cd
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-04-30 02:11:02 -0700

    Add files via upload

---
+++ Week-2/Python for Data Science/data-sum-of-three/sum_of_three.py
@@ -0,0 +1,9 @@
+"""
+First code in bootcamp
+"""
+
+def sum3(first_number, second_number, third_number):
+    """
+    Returning sum of numbers
+    """
+    return first_number+second_number+third_number
