91802a6bf768ac7ad6e1f934669f3268dfb09867
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-04-30 01:37:09 -0700

    Add files via upload

---
+++ Week-1/data-git/today.py
@@ -0,0 +1,12 @@
+"""A module computing buddy pair names for the day"""
+
+
+def my_name_is():
+    'function description'
+    my_name = 'gulecsec'
+    return my_name
+
+def my_buddy_is():
+    'function description'
+    my_buddy_name = 'gida'
+    return my_buddy_name
